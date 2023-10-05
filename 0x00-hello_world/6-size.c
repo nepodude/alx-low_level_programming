2f7dae6ede9de36678742098f3c9b1bcfb619286
nepodude <nepo.jean73@gmail.com>
2023-10-05 16:59:42 -0700

    I got it

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Some program that prints
+ * Return 0 (Succes)
+ */
+int main(void)
+{
+  char a;
+  int b;
+  long int c;
+  long long int d;
+  float f;
+printf(“char size: %lu byte(s)\n“, (unsigned long)sizeof(a) );
+printf(“int size: %lu byte(s)\n“, (unsigned long)sizeof(b) );
+printf(“long int size: %lu byte(s)\n“, (unsigned long)sizeof(c) );
+printf(“long long int size: %lu byte(s)\n“, (unsigned long)sizeof(d) );
+printf(“float size: %lu byte(s)\n“, (unsigned long)sizeof(x) );
+}
