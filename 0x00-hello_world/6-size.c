21cd08245fb85bcfd2c1ac779e54d2b022c975bb
nepodude <nepo.jean73@gmail.com>
2023-10-05 17:07:10 -0700

    I got it

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,10 @@ int main(void)
   int b;
   long int c;
   long long int d;
-  float f;
-printf(“char size: %lu byte(s)\n“, (unsigned long)sizeof(a));
-printf(“int size: %lu byte(s)\n“, (unsigned long)sizeof(b));
-printf(“long int size: %lu byte(s)\n“, (unsigned long)sizeof(c));
-printf(“long long int size: %lu byte(s)\n“, (unsigned long)sizeof(d));
-printf(“float size: %lu byte(s)\n“, (unsigned long)sizeof(x));
+  float f
+printf("char size: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("int size: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("long int size: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("long long int size: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("float size: %lu byte(s)\n", (unsigned long)sizeof(x));
 }
