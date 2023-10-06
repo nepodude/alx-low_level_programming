111f291304d8f2223ffadac1d7c6eb0b0f6d0c63
nepodude <nepo.jean73@gmail.com>
2023-10-06 13:29:40 -0700

    I am on it

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * main - prints all single digit numbers of base
  * 10 starting from 0, followed by a new line
- * This is one of the very easiest codes 
+ * This is one of the very easiest codes
  * Return: Always 0 (Success)
  */
 int main(void)
