74828337c1a2589128c94a0c86475b9e02120dd3
nepodude <nepo.jean73@gmail.com>
2023-10-06 13:01:47 -0700

    I am on it

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints everything in lower case.
+ * All alphabets are printed
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
