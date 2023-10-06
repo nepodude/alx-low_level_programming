2c4412bfdc39b7dc9f3433dbcc3705f7ce68050e
nepodude <nepo.jean73@gmail.com>
2023-10-05 17:08:30 -0700

    I got it

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,4 +15,5 @@ printf("int size: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("long int size: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("long long int size: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("float size: %lu byte(s)\n", (unsigned long)sizeof(x));
+return 0;
 }
