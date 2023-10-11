f22d99d79fc06ff3ef5a2d62623635ad50c6d3fb
nepodude <nepo.jean73@gmail.com>
2023-10-11 15:54:59 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - writes the character c to stdout
+ * The character to print
+ * Return: On success 1.
+ */
+int main(void)
+{
+for (int chara = 97; chara <= 97 + 25; chara++)
+{
+_putchar(chara);
+}
+_putchar('\n');
+return (0);
+}
