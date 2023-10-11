b3f85d39224697bb627de460f9ce7b2a85a961c0
nepodude <nepo.jean73@gmail.com>
2023-10-11 05:38:57 -0700

    got this

---
+++ 0x01-variables_if_else_while/100-print_comp3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main Entry point
+ * printing combination of digits 
+ * return 0: success
+*/
+int main()
+{
+int x = 0;
+for (; x <= 9; x ++)
+{
+for (int y = x + 1; y <= 9; y ++){
+if (x != y)
+{
+putchar(x + 48);
+putchar(y+ 48);
+putchar(',');
+putchar(' ');
+}
+}
+}
+return 0;
+}
