e1340922b331d12e0c10eb0117b7897c97550bc3
nepodude <nepo.jean73@gmail.com>
2023-10-11 17:42:26 -0700

    this is savage

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,13 @@
 
 /**
  * _islower - this is an outstanding function
- * The character to print
- * Return: On success be happy.
+ * @c: char type letter
+ * Return: 1 if lowercase, 0 if no lower case
  */
 int _islower(int c)
-        {
-	        if (c >= 97 && c <= 122)
-			return (1);
-		else
-			return (0);
-	}
-
+{
+if (c >= 97 && c <= 122)
+return (1);
+else
+return (0);
+}
