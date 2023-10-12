fb284ddd62d5fe085ffffb2d2ca237750c47969e
nepodude <nepo.jean73@gmail.com>
2023-10-12 10:34:15 -0700

    I changed many things to see wat works

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - will see positivity or negativity of integer n
+ * and if it is equal to 0 too.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
+}
