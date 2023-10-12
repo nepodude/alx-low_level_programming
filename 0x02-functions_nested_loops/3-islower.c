bb5ab71bdea8f37f9b4bbdf111bcbc38e6d35a68
nepodude <nepo.jean73@gmail.com>
2023-10-11 17:14:54 -0700

    print yes or no

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,10 +6,10 @@
  * Return: On success be happy.
  */
 int _islower(int c)
-{
-if (islower(c))
-{
-return (1);
-}
-return (0);
-}
+	{
+		if (c >= 'a' && c <= 'z')
+			return (1);
+		else
+			return (0);
+	}
+
