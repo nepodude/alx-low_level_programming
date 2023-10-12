15a5f88c9d92c324d0c87ad6fb348de20e050c95
nepodude <nepo.jean73@gmail.com>
2023-10-11 17:07:14 -0700

    print 0 or 1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - this is an outstanding function
+ * The character to print
+ * Return: On success be happy.
+ */
+int _islower(int c)
+{
+if (islower(c) == TRUE)
+{
+return (1);
+}
+else
+return (0);
+}
