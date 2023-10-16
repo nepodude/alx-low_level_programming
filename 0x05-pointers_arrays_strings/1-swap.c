0b22830967d55155ca6ebe1dd4d4130eec4b1e4e
nepodude <nepo.jean73@gmail.com>
2023-10-16 11:55:53 -0700

    done now hhh

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,12 +3,11 @@
  * swap_int - check the code
  * @a: input
  * @b: input
- * Return: Always 0.
 */
 void swap_int(int *a, int *b)
 {
 int hidden;
 hidden = *a;
-*b = hidden;
 *a = *b;
+*b = hidden;
 }
