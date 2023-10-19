ce4d45d46377fb82a5f130a729345a4e24d5f732
nepodude <nepo.jean73@gmail.com>
2023-10-19 09:49:43 -0700

    I wrote counter1++ instead of counter1 only

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,47 +1,26 @@
 #include "main.h"
 /**
- * _strncat - concat the first n characters of string src
- * @dest:char input
- * @src:char input
- * @n: a regulating integer.
- * Return:char out of the function
+ * _strncat - Concatenate the first n characters of string src to dest
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Number of characters to concatenate
+ * Return: Pointer to the resulting string (dest)
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int counter = 0;
-while (*src != '\0')
-{
-counter++;
-}
 char *s = dest;
-if (n > counter)
-{
 while (*dest)
-{
 dest++;
-}
-while (*src)
+if (n > 0)
+{
+while (*src && n > 0)
 {
 *dest = *src;
 dest++;
 src++;
-}
-}
-else
-{
-while (*dest)
-{
-dest++;
-}
-int counter1 = 0;
-while (counter1 < n)
-{
-*dest = *src;
-dest++;
-src++;
-counter1++;
+n--;
 }
 }
 *dest = '\0';
-return (s);
+return s;
 }
