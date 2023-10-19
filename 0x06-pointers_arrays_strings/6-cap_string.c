615ca227b3452794dfc6e295407f62ced67eceb9
nepodude <nepo.jean73@gmail.com>
2023-10-19 16:29:06 -0700

    this will work

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+ #include "main.h"
+/**
+ * cap_string - change a strin to all upper case.
+ * @s: a string pointer input
+ * Return: the string s it's self
+ */
+char *cap_string(char *s)
+{
+char *beginning = s;
+while (*s != '\0')
+{
+if (*s == ' ' || *s == '\t' || *s == '\n' || *s == ','
+|| *s == ';' || *s == '.' || *s == '!' || *s == '?' || *s == '"'
+|| *s == '(' || *s ==  ')' || *s == '{' || *s == '}')
+{
+if (*(s + 1) >= 97 && *(s + 1) <= 97 + 26)
+{
+*(s + 1) -= 32;
+}
+}
+}
+return (beginning);
+}
