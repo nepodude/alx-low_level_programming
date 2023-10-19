4ade3b3a855efd1d600f019be225de17341da61e
nepodude <nepo.jean73@gmail.com>
2023-10-19 06:25:04 -0700

    I restarted everything

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+* _strcat - concatenating two  strings
+ * @dest:char input
+ * @src:char input
+ * Return:char out of the function
+ */
+char *_strcat(char *dest, char *src)
+{
+while (*dest)
+{
+dest++;
+}
+while (*src)
+{
+*dest = *src;
+dest++;
+src++;
+}
+return (dest);
+}
