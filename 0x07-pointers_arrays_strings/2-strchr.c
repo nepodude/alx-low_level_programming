75872bb9a086e19440c9c7cfcea860a6cad59bb2
nepodude <nepo.jean73@gmail.com>
2023-10-23 15:40:18 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strchr - locates a character in a string
+ * @s: string to search
+ * @c: char to find
+ * Return: a pointer to the first occurrence of c
+ */
+char *_strchr(char *s, char c)
+{
+char *indicator = s;
+while (*s != '\0')
+{
+if (*s == 'c')
+{
+return (indicator);
+break;
+}
+else
+{
+continue;
+}
+return (NULL);
+s++;
+}
+}
