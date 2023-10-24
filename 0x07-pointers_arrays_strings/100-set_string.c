734b41f9965bfd9d4ab03b5f5cd20e97e7035ef2
nepodude <nepo.jean73@gmail.com>
2023-10-23 19:14:59 -0700

    I got this

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,7 +2,7 @@
 
 /**
  * set_string - it changes valut to char  
- *@s: pointer to pointer we need to set to
+ * @s: pointer to pointer we need to set to
  * @to: string to set
  */
 void set_string(char **s, char *to)
