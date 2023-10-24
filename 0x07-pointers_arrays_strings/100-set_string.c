0a8685d663325e113561ca19c173610fe29be940
nepodude <nepo.jean73@gmail.com>
2023-10-23 19:11:00 -0700

    I got this

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * set_string - it changes valut to char  
+ *@s: pointer to pointer we need to set to
+ * @to: string to set
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
