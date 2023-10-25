937b1e94d457674b798b069609632e386ebf57bc
nepodude <nepo.jean73@gmail.com>
2023-10-24 23:06:17 -0700

    reviewed my work

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,11 @@
 */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar,
-2015-10-19\n";
-write(2, message, 60);
-return (1);
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of an double: %lu byte(s)\n", sizeof(double));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
 }
