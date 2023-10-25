e294c23a35c74385c4db5cd2c4007140be1e69fa
nepodude <nepo.jean73@gmail.com>
2023-10-25 12:42:36 -0700

    debugged

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,15 +5,14 @@
  */
 void _puts_recursion(char *s)
 {
-if (*s == '/0')
+if (*s == '\0')
 {
-_putchar ('/n');
-break;
+_putchar ('\n');
 }
-if (*s != '/0')
+if (*s != '\0')
 {
 _putchar ('s');
 s++;
-_puts_recursion (char *s)
+_puts_recursion(char s);
 }
 }
