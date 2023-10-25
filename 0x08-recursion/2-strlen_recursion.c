b3b4e5f412fda78c97ed9e36b160d6c1d7195c3e
nepodude <nepo.jean73@gmail.com>
2023-10-25 13:58:20 -0700

    debugged

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-int length = 0;
 if (*s)
 {
-length++;
-s++;
-_strlen_recursion(s);
-return (length);
+return (1 + _strlen_recursion(s + 1));
+}
+else
+}
+return (0);
 }
 }
