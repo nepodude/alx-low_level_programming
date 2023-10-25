f1428c59ae9e9a24873edd05412c3ea47aa6f1fb
nepodude <nepo.jean73@gmail.com>
2023-10-24 23:09:28 -0700

    removed spce

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
 */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const char *message = "and that piece of art is useful\" -
+Dora Korpar, 2015-10-19\n";
 write(2, message, 60);
 return (1);
 }
