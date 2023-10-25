4f88d394f47db1cab98811092398cc68bc78ec44
nepodude <nepo.jean73@gmail.com>
2023-10-25 12:57:35 -0700

    debugged

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * _puts_recursion - prints a string recursively
- * @s: is unput string to print
+ * @s: is input string to print
  */
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
 {
-_putchar ('\n');
+_putchar('\n');
 }
-if
+else
 {
-_putchar (s);
+_putchar(*s);
 s++;
 _puts_recursion(s);
 }
