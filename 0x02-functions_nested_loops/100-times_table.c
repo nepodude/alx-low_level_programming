d62a3b3602cde55a6f444dbabf3a97700dfc72a3
nepodude <nepo.jean73@gmail.com>
2023-10-24 23:34:22 -0700

    redid task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,47 @@
 #include "main.h"
-
 /**
  * print_times_table - Print the `n` times table, starting with 0.
  * Description: If `n` is greater than 15 or less than 0, print nothing.
  * @n: int type number
  */
-
 void print_times_table(int n)
 {
-	int x = 0, y, z;
-
-	if (n > 15 || n < 0)
-		return;
-	while (x <= n)
-	{
-		for (y = 0; y <= n; y++)
-		{
-			z = x * y;
-			if (z > 99)
-			{
-				_putchar(z / 100 + '0');
-				_putchar((z / 10 % 10) + '0');
-				_putchar(z % 10 + '0');
-			}
-			else if (z > 9)
-			{
-				_putchar(' ');
-				_putchar(z / 10 + '0');
-				_putchar(z % 10 + '0');
-			}
-			else if (y != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
-			}
-			else
-				_putchar(z + '0');
-
-			if (y != n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-		x++;
-	}
+int i = 0;
+int j, product;
+if (n > 15 || n < 0)
+return;
+while (i <= n)
+{
+for (j = 0; j <= n; j++)
+{
+product = i * j;
+if (product > 99)
+{
+_putchar(product / 100 + '0');
+_putchar((product / 10 % 10) + '0');
+_putchar(product % 10 + '0');
+}
+else if (product > 9)
+{
+_putchar(' ');
+_putchar(product / 10 + '0');
+_putchar(product % 10 + '0');
+}
+else if (j != 0)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(product + '0');
+}
+else
+_putchar(product + '0');
+if (j != n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+i++;
+}
 }
