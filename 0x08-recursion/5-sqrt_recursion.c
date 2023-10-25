7e05298ced9389527099707d3167b639635dc6db
nepodude <nepo.jean73@gmail.com>
2023-10-25 16:07:02 -0700

    now gotta work

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,38 +1,33 @@
 #include "main.h"
-
 int helper(int n, int i);
-
 /**
  *_sqrt_recursion - prints final thing
  * @n: input integer.
  * Return: returns integer squareroot
  */
-
 int _sqrt_recursion(int n)
 {
 return (helper(n, 0));
 }
-
 /**
- * -helper - recursively find int sqrt
+ * helper - recursively find int sqrt
  * @n: input to find squareroot for.
- * @i; input counter
+ * @i: input counter
  * Return: integer square root.
  */
-
 int helper(int n, int i)
 {
-  if (i * i ==  n)
-    {
-      return (i);
-    }
-  else if (i * i > n || n < 0)
-    {
-      return (-1);
-    }
-  else if (i * i < n)
-    {
-      return (helper(n, i + 1));
-    }
-  return (-1);
+if (i * i ==  n)
+{
+return (i);
+}
+else if (i * i > n || n < 0)
+{
+return (-1);
+}
+else if (i * i < n)
+{
+return (helper(n, i + 1));
+}
+return (-1);
 }
