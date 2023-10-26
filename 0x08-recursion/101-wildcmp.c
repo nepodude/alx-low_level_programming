e7f0ce3056059771367388ba10ac1b8234ef176f
nepodude <nepo.jean73@gmail.com>
2023-10-25 18:40:21 -0700

    last one

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,8 +8,7 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-if ((*s1 == *s2) || ((*s1 != *s2) && *s2 == '*'))
-&&(*s1 && *s2))
+if ((*s1 == *s2) || ((*s1 != *s2) && (*s2 == '*')))
 {
 wildcmp(s1 + 1, s2 + 1);
 return (1);
