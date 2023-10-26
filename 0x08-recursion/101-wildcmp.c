95ff251aeb67f5d5fb81ed32d448fe65187228ff
nepodude <nepo.jean73@gmail.com>
2023-10-25 18:36:42 -0700

    last one

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * wildcmp - compares two strings
+ * @s1: the normal string
+ * @s2: the special string containing "*"
+ *
+ * Return: 1 if identical, else 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+if ((*s1 == *s2) || ((*s1 != *s2) && *s2 == '*'))
+&&(*s1 && *s2))
+{
+wildcmp(s1 + 1, s2 + 1);
+return (1);
+}
+return (0);
+}
