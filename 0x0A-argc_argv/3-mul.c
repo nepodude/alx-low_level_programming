e6d771d418b25b138cfbfbdc95b73ab05fbcd3df
nepodude <nepo.jean73@gmail.com>
2023-10-27 16:10:54 -0700

    loek

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,27 @@
 #include <stdio.h>
+#include <stdio.h>
+/**
+ * _atoi - acts like real function atoi
+ * @s: input
+ * Return: integer multiple.
+ */
+int _atoi(char *s)
+{
+int result = 0;
+int sn = 1;
+int final = result * sn;
+if (*s == '-')
+{
+sn = -1;
+s++;
+}
+while (*s >= '0' && *s <= '9')
+{
+result = result * 10 + (*s - '0');
+s++;
+}
+return (final);
+}
 /**
  * main - prints the product
  * @argc: number of arguments
@@ -8,16 +31,14 @@
  */
 int main(int argc, char *argv[])
 {
-if (argc > 0)
+if (argc != 3)
 {
-int result;
-result = arg[1] * arg[2];
-printf("%d\n", result);
-}
-else
-{
-printf("Error");
+printf("Error\n");
 return (1);
 }
+int num1 = _atoi(argv[1]);
+int num2 = _atoi(argv[2]);
+int product = num1 * num2;
+printf("%d\n", product);
 return (0);
 }
