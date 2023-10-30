975d5147274e10241b21ab401e242b807ba518a7
nepodude <nepo.jean73@gmail.com>
2023-10-29 22:42:19 -0700

    nepo

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints number of coins
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+  int remainder1, remainder2, reaminder3, remainder4;
+  remainder1 = atoi(argv[1]) % 25;
+  remainder2 = reaminder1 % 10;
+  remainder3
+  if(argc != 2)
+    {
+      printf("Error\n");
+      return (1);
+    }
+  if (argv[1] < 0)
+    {
+      printf("0\n");
+      return (0);
+    }
+}
