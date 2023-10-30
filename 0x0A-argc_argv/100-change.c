f666b8136730581150d011b5cee8c89cbaf44865
nepodude <nepo.jean73@gmail.com>
2023-10-29 22:48:49 -0700

    this will work

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,18 +9,22 @@
  */
 int main(int argc, char *argv[])
 {
-  int remainder1, remainder2, reaminder3, remainder4;
-  remainder1 = atoi(argv[1]) % 25;
-  remainder2 = reaminder1 % 10;
-  remainder3
-  if(argc != 2)
-    {
-      printf("Error\n");
-      return (1);
-    }
-  if (argv[1] < 0)
-    {
-      printf("0\n");
-      return (0);
-    }
+int remainder1, remainder2, reaminder3, remainder4, answer;
+remainder1 = atoi(argv[1]) % 25;
+remainder2 = remainder1 % 10;
+remainder3 = remainder2 % 5;
+remainder4 = remainder3 % 2;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+if (argv[1] < 0)
+{
+printf("0\n");
+return (0);
+}
+answer = remainder1 + remainder2 + remainder3 + remainder4;
+printf("%d\n", answer);
+return (0);
 }
