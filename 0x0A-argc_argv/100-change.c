6cd35c4df68a3e07fb14923b6afaf3291a62c9d0
nepodude <nepo.jean73@gmail.com>
2023-10-29 23:03:42 -0700

    done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,21 +10,22 @@
 int main(int argc, char *argv[])
 {
 int remainder1, remainder2, remainder3, remainder4, answer;
-remainder1 = atoi(argv[1]) % 25;
-remainder2 = remainder1 % 10;
-remainder3 = remainder2 % 5;
-remainder4 = remainder3 % 2;
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-if (argv[1] < 0)
+remainder1 = atoi(argv[1]) % 25;
+remainder2 = remainder1 % 10;
+remainder3 = remainder2 % 5;
+remainder4 = remainder3 % 2;
+if (atoi(argv[1]) < 0)
 {
 printf("0\n");
 return (0);
 }
-answer = remainder1 / 25 + remainder2  / 10 + remainder3 / 5 + remainder4;
+answer = atoi(argv[1]) / 25 + remainder1 / 10 + remainder2  / 5
++ remainder3 / 2 + remainder4;
 printf("%d\n", answer);
 return (0);
 }
