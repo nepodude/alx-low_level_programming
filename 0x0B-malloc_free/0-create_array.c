ccc0eaa4a3650bc014a5c475bd1f05cfc41208d3
nepodude <nepo.jean73@gmail.com>
2023-10-31 16:30:58 -0700

    first one

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ unsigned int i;
 char *array;
 if (size == 0)
 return (NULL);
-array = malloc(sizeof(char));
+array = malloc(sizeof(char)*size);
 if (array == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
