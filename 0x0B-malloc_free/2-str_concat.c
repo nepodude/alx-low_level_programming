9861f4b927053a05c44692217fc86edea1866529
nepodude <nepo.jean73@gmail.com>
2023-10-31 17:14:35 -0700

    Let's concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,16 @@
 char *str_concat(char *s1, char *s2)
 {
 char *newstring;
-int x;
+int first, second;
+if (s1 != NULL)
+first = strlen(s1);
+if (s2 != NULL)
+second = strlen(s2);
 x = strlen(s1) + strlen(s2) + 1;
 newstring = malloc(x *sizeof(char));
-strcat(s1, s2);
-return (s1);
+if (s1 != NULL)
+strcpy(newstring, s1);
+if (s2 != NULL)
+strcpy(newstring, s2);
+return (newstring);
 }
