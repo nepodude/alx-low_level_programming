4d6ee0cda1fde8c33b1e87d264e0fbf562a40691
nepodude <nepo.jean73@gmail.com>
2023-11-02 15:31:01 -0700

    fixed a bug

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,8 +18,10 @@ if (s2 == NULL)
 s2 = "";
 while (*s1)
 len1++;
+s1++;
 while (*s2)
-len2;
+len2++;
+s2++;
 if (n >= len2)
 {
 s3 = malloc(len1 + len2 + 1);
@@ -33,7 +35,7 @@ for (i = 0; i < len1; i++)
 s3[i] = s1[i];
 for (j = 0; j < len2; j++)
 s3[i + j] = s2[j];
-s3[i + j + 1] = '\0';
+s3[i + j] = '\0';
 return (s3);
 }
 }
