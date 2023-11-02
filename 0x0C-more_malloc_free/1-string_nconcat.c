b0e13f404148dd86d7fe2e383690b24353d5f173
nepodude <nepo.jean73@gmail.com>
2023-11-02 15:50:58 -0700

    fixed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,16 +16,10 @@ if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-while (*s1)
-{
+while (s1[len1] != '\0')
 len1++;
-s1++;
-}
-while (*s2)
-{
+while (s2[len2] != '\0')
 len2++;
-s2++;
-}
 if (n >= len2)
 {
 s3 = malloc(len1 + len2 + 1);
