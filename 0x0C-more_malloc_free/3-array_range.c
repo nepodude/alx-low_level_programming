413c6172ae815790eca60aa90390896382fc5e6b
nepodude <nepo.jean73@gmail.com>
2023-11-02 17:16:41 -0700

    fixed everything

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* array_range - creates an array in increaseing order
+* @min: smallest integer
+* @max: lagrest integer
+*
+* Return: pointer to the starting thing
+*/
+int *array_range(int min, int max)
+{
+int *tester;
+if (min > max)
+return (NULL);
+tester = malloc(max - min + 1);
+if (tester == NULL)
+return (NULL);
+else
+for (int i = 0; i <= max - min; i++)
+tester[i] = i + min;
+return (tester);
+}
