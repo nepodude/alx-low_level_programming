b1383a1ef470dd2cddfcf4ef1e1a28e13ed6f0f3
nepodude <nepo.jean73@gmail.com>
2023-11-02 18:54:47 -0700

    now

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,33 +10,40 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-char *new_ptr;
-char *src = ptr;
-char *dst;
-unsigned int min_size;
-if (old_size == new_size)
-return (ptr);
-if (ptr == NULL)
-{
-ptr = malloc(new_size);
-return (ptr);
-}
-if (new_size == 0 && ptr != NULL)
-{
-free(ptr);
-return (NULL);
-}
-new_ptr = malloc(new_size);
-dst = new_ptr;
-if (new_ptr)
-{
-if (old_size < new_size)
-min_size = old_size;
-else
-min_size = new_size;
-for (unsigned int i = 0; i < min_size; i++)
-dst[i] = src[i];
-free(ptr);
-}
-return (new_ptr);
+char *ptr1;
+	char *old_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	free(ptr);
+	return (ptr1);
 }
