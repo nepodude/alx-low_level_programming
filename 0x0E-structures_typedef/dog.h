65c63cb05e5fb75fd49d28070de4bc5718e41c73
nepodude <nepo.jean73@gmail.com>
2023-11-06 14:54:57 -0800

    nepo

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog's information
+ * @name: of the dog
+ * @age: of the dog
+ * @owner: dog's owner
+ */
+typdef struct
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
