3d0c96e4fdfca1a444d69df40ab8a140a0990f3d
nepodude <nepo.jean73@gmail.com>
2023-11-06 16:47:22 -0800

    bing's help

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <dog.h>
 /**
  * new_dog - creates new frind dog hahaha
@@ -11,34 +10,28 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog;
-    int i, j;
-
-    i = strlen(name);
-    j = strlen(owner);
-
-    dog = malloc(sizeof(dog_t));
-    if (dog == NULL)
-        return (NULL);
-
-    dog->name = malloc(sizeof(char) * (i + 1));
-    if (dog->name == NULL)
-    {
-        free(dog);
-        return (NULL);
-    }
-
-    dog->owner = malloc(sizeof(char) * (j + 1));
-    if (dog->owner == NULL)
-    {
-        free(dog->name);
-        free(dog);
-        return (NULL);
-    }
-
-    strcpy(dog->name, name);
-    strcpy(dog->owner, owner);
-    dog->age = age;
-
-    return (dog);
+dog_t *dog;
+int i, j;
+i = strlen(name);
+j = strlen(owner);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+dog->name = malloc(sizeof(char) * (i + 1));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+dog->owner = malloc(sizeof(char) * (j + 1));
+if (dog->owner == NULL)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+}
+strcpy(dog->name, name);
+strcpy(dog->owner, owner);
+dog->age = age;
+return (dog);
 }
