84529c0e94fea22ad886797c4e1d20449d18ad97
nepodude <nepo.jean73@gmail.com>
2023-11-06 16:52:46 -0800

    rogorous

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dog.h>
+
+#include "main.h"
+/**
+ * _strlen - check the code
+ * @s: input
+ * Return: Always 0.
+*/
+int _strlen(char *s)
+{
+int counter  = 0;
+while (s[counter] != '\0')
+{
+counter++;
+}
+return (counter);
+}
+
+#include "main.h"
+
+/**
+ * char *_strcpy - this is a thing that copies the string pointed to by src
+ * @dest: will copy to this
+ * @src: will  copy from this
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	while (*(src + a) != '\0')
+	{
+		a++;
+	}
+	for ( ; b < a ; b++)
+	{
+		dest[b] = src[b];
+	}
+	dest[a] = '\0';
+	return (dest);
+}
+
 /**
  * new_dog - creates new frind dog hahaha
  * @name: name of the dog
@@ -10,28 +52,32 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *dog;
-int i, j;
-i = strlen(name);
-j = strlen(owner);
-dog = malloc(sizeof(dog_t));
-if (dog == NULL)
-return (NULL);
-dog->name = malloc(sizeof(char) * (i + 1));
-if (dog->name == NULL)
-{
-free(dog);
-return (NULL);
-}
-dog->owner = malloc(sizeof(char) * (j + 1));
-if (dog->owner == NULL)
-{
-free(dog->name);
-free(dog);
-return (NULL);
-}
-strcpy(dog->name, name);
-strcpy(dog->owner, owner);
-dog->age = age;
-return (dog);
+	dog_t *dog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
 }
