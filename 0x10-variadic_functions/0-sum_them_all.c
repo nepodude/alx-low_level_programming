d842d6d6b5425be75980eee5c39f6d982fa3150b
nepodude <nepo.jean73@gmail.com>
2023-11-09 11:06:22 -0800

    this was taught

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,10 +11,8 @@ unsigned int i;
 unsigned int sum = 0;
 va_list args;
 va_start(args, n);
-for (i = 0; i < n; ++i)
-{
+for (i = 0; i < n; i++)
 sum += va_arg(args, int);
-}
 va_end(args);
 return (sum);
 }
