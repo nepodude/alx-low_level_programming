1c61c338b0bfeb62d2de564b85ee7cf08ec51fcb
nepodude <nepo.jean73@gmail.com>
2023-11-09 10:28:48 -0800

    debugged

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,7 +10,8 @@ int sum_them_all(const unsigned int n, ...)
 va_list args;
 va_start(args, n);
 int sum = 0;
-for (unsigned int i = 0; i < n; ++i)
+unsigned int i;
+for (i = 0; i < n; ++i)
 {
 int value = va_arg(args, int);
 sum += value;
