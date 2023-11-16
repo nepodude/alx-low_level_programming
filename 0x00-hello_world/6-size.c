5422406d267729f469c7b4cd14be4537c59cc310
root <root@Nepo.localdomain>
2023-11-16 11:34:17 +0200

    changed some stuffs

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,16 @@
 */
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of an double: %lu byte(s)\n", sizeof(double));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
+int a;
+char b;
+long int c;
+long long int d;
+float e;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+printf("Size of an double: %lu byte(s)\n", (unsigned long)sizeof(double));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 return (0);
 }
