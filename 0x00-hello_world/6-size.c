4c3c96038fe3052b30a7918913ef11b2cc13e6b0
root <root@Nepo.localdomain>
2023-11-16 11:39:56 +0200

    nanone

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,17 @@
 */
 int main(void)
 {
-int a;
-char b;
-long int c;
-long long int d;
-float e;
+	int a;
+	char b;
+	long int c;
+	long long int d;
+	float e;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
 printf("Size of an double: %lu byte(s)\n", (unsigned long)sizeof(double));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+printf("Size of a long long int: %lu byte(s)\n", 
+(unsigned long)sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 return (0);
 }
