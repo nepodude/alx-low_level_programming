05b981cb3f58ac171231517fa74d7092e9a26b62
root <root@Nepo.localdomain>
2023-11-18 01:44:37 +0200

    now

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,14 +16,14 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-		return(NULL);
+		return (NULL);
 	new_node->str = strdup(str);
 	if (!new_node->str)
 	{
 		free(new_node);
-		return(NULL);
+		return (NULL);
 	}
-	new_node->len = strlen(*(new_node).str);
+	new_node->len = strlen(new_node->str);
 	new_node->next = *head;
 	*head = new_node;
 
