8115a8a561476d3ef44707924799c3c5c8060195
root <root@Nepo.localdomain>
2023-11-18 01:26:06 +0200

    nice one

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node - appends a node at start of list.
+ * @head: input list to manipulate
+ * @str: input string to add.
+ * Return: the new list formed.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return(NULL);
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return(NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
