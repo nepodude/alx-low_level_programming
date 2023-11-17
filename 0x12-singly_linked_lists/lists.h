df3fdaf3aea685f5db7c62cd59ce7eef3e739986
root <root@Nepo.localdomain>
2023-11-18 00:41:12 +0200

    debugged fifth time

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,22 @@
 #ifndef	LISTS_H
 #define LISTS_H
 
-typedef struct list_s {
-        char *str;
-        unsigned int len;
-        struct list_s *next;
-}list_t;
+/**
+ * struct list_s - singly linked list
+ * @str: a string given space
+ * @len: bytes of the string
+ * @next: pointer to next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
