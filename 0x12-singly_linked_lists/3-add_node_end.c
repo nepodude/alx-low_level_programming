ada337c43c4bd3080cf4af553fed2c9f9549071a
root <root@Nepo.localdomain>
2023-11-18 02:09:22 +0200

    got it now

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - appends lest @head with a goddamn node.
+ * @head: a list to play with adding the thing
+ * @str: a string to use in the appending ritual
+ *
+ * Return: the goddamn location of appended node hahaha.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_list;
+
+	while (*head)
+	{
+		*head++;
+	}
+
+	new_list = malloc(sizeof(list_t));
+	if (!new_list)
+		return (NULL);
+	new_list->str = strdup(str);
+	if (new_list->str == NULL)
+	{
+		free(new_list);
+		return (NULL);
+	}
+	new_list->next = *head;
+	new_list->len = strlen(new_list->str);
+	return (new_list);
+
+}
