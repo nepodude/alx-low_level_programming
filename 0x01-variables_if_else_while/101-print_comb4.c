2519ed045abd276b70289210fbbc2a6aedd35692
root <root@Nepo.localdomain>
2023-11-18 17:09:36 +0200

    done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - function that prints distinct combinations of digits
+ * in increasing order and and 11, 22,.. are not allowed hahaha.
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = x + 1; y <= 9; y++)
+		{
+			for (z = 0; z <= 9; z++)
+			{
+				 putchar(x + 48);
+				 putchar(y + 48);
+				 putchar(z + 48);
+				 if (x != 7)
+				 {
+					 putchar(',');
+					 putchar(' ');
+				 }
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
