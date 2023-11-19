947d0c4ef4a0b150889ba54c566f45a3ccf602c3
root <root@Nepo.localdomain>
2023-11-19 08:23:25 +0200

    repeated now

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,26 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define(PASSWORD_LENGTH) = (10)
+
+/**
+ * main - prints a random password
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	const char possibilities[] = "abcdefghijklmnopqrstuvwxyz
+		ABCDEFGHIJKLMKNOPQRSTUVWXYZ0123456789";
+	char password[PASSWORD_LENGTH + 1];
+	unsigned int i = 0;
+
+	srand(time(NULL));
+	for (; i < PASSWORD_LENGTH; i++)
+	{
+		password[i] = possibities[rand() % (sizeof(possibilities) - 1)];
+	}
+
+	printf("%s", password);
+	return (0);
+}
