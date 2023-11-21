5c6be9a9ace89727c05f54b9dc08f74ef275f2dd
root <root@Nepo.localdomain>
2023-11-21 23:30:49 +0200

    nice and clean

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,11 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t counter;
+	counter = 0;
 
 	while (h != NULL)
 	{
-		printf("%d", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		counter++;
 	}
