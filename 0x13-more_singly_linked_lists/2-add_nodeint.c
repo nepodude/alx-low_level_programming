a1af0a8498685c3b12a38e93afff7edfc5cbe0ba
root <root@Nepo.localdomain>
2023-11-22 00:17:50 +0200

    okay

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds an integer to the beginning
+ * @head: input list
+ * @n: constant to be put in our node
+ * Return: Address of the new element and NULL on failuree.
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = (listint_t *)malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
