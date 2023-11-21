b9eb12e60dc80ea1567802666a5c51ca35c11dd5
root <root@Nepo.localdomain>
2023-11-22 00:35:56 +0200

    okay

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint_end - appends a node at start of list.
+ * @head: input list to manipulate
+ * @n: input integer to add.
+ * Return: the address of new inserted node.
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_mode == NULL)
+		return (NULL);
+	while (*head != NULL)
+	{
+		*head = *head->next;
+	}
+	new_node->n = n;
+	*head = new_node;
+	new_node->next = NULL;
+
+	return (new_node);
+}
