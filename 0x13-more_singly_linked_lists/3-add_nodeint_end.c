7020974db7144051f011cff173437573e0601ac4
root <root@Nepo.localdomain>
2023-11-22 01:03:21 +0200

    okay

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,11 +10,13 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_nodem, *temp;
+	listint_t *new_node, *temp;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
+	if (*head == NULL)
+		*head = new_node;
 	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
