082e4e020bc4371638650fa8487a991288f866b2
root <root@Nepo.localdomain>
2023-11-21 23:41:20 +0200

    easy one

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include <lists.h>
+
+/**
+ * listint_len - prints all members of a list haha
+ * @h: input list
+ * Return: the number of nodes our list had yeyeye.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t counter;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
