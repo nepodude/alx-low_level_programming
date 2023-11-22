a85f96863ff2cddec842e697ab795b5741ed2be6
root <root@Nepo.localdomain>
2023-11-22 02:23:18 +0200

    okay

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,8 +2,23 @@
 #include <stdlib.h>
 
 /**
- *  * pop_listint - delete the hell out of it hahahaht.
- *   * @head: input list to play with.
- *    * Return: returns int of formal head.
- *     */
+ * sum_listint - delete the hell out of it hahahaht.
+ * @head: input list to play with.
+ * Return: returns integer sum of all things.
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	sum += head;
+	return (sum);
+}
 
