c77087e65586620accc51c56c77352617e05a97c
root <root@Nepo.localdomain>
2023-11-22 08:24:26 +0200

    debugged

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *temp;
 	unsigned int i;
 
-	if (idx > 0 || (*head) == NULL)
+	if (idx > 0 && (*head) == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
