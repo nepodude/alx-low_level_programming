4e2f74e5df458bff23cd62af0bd83c515a9752ef
root <root@Nepo.localdomain>
2023-11-24 23:20:23 +0200

    okay

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+
+unsigned int powering (unsigned int a,unsigned int b);
+
+/**
+ * binary_to_unit - returns an equivalent number in decimal.
+ * @b: an input pointer that will point to whatever the first character of
+ * input string is.
+ * Return: unsigned int output after computation.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0, length = 0, helper;
+	const char *temp = b;
+
+	if (b == NULL)
+		return (0);
+	while (*temp != '\0')
+	{
+		if (*temp != '0' && *temp != '1')
+			return (0);
+		length++;
+		temp++;
+	}
+	temp = b;
+	for (helper = 0; helper < length; helper++)
+	{
+		result += (temp[helper] - '0') * powering(2, (length - helper - 1));
+	}
+	return(result);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
 #ifndef main_h
-#define main_hahaha
+#define main_h
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -8,5 +8,23 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+
+/**
+ * powering - returns power.
+ * @a: base
+ * @b: power
+ * Return: result of the power
+ */
+
+static inline unsigned int powering(unsigned int a, unsigned int b)
+{
+	unsigned int answer = 1;
+	
+	for (; b > 0; b--)
+	{
+		answer *= a;
+	}
+	return (answer);
+}
 
 #endif
