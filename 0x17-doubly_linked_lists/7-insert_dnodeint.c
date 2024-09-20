988d6310f5cb41616f58965c81d883f9bc7521c8
nepodude <nepo.jean73@gmail.com>
2024-09-20 15:29:18 +0200

    okay

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	if (h == NULL)
-		return (NULl);
+		return (NULL);
 
 	helper = *h;
 	while (helper != NULL)
