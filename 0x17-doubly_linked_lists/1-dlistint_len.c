3d9a6afe92c281e492a436449ad37bc281f39b98
nepodude <nepo.jean73@gmail.com>
2024-09-20 12:09:41 +0200

    okay I got it

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* dlistint_len - returns the number of elements
+* @h: pointer to the head.
+* Return: number of elements.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *helper;
+	int counter = 0;
+
+	helper = h;
+	while (helper != NULL)
+	{
+		counter++;
+		helper = helper->next;
+	}
+	return (counter);
+}
