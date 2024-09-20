dd75e0b6b8241c92f2b37eafaab30be1574a7615
nepodude <nepo.jean73@gmail.com>
2024-09-20 16:06:56 +0200

    okay

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* delete_dnodeint_at_index - inserts a node somewhere.
+* delete_dnodeint_at_index - deletes a node somewhere.
 * @head: first node.
 * @index: an index where to delete node.
 * Return: -1 on failure and 1 otherwise.
@@ -10,22 +10,34 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
-	dlistint_t *helper, *nodehelper;
+	dlistint_t *helper, *node_to_delete;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	helper = *head;
+	if (index == 0)
+	{
+		node_to_delete = *head;
+		*head = node_to_delete->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(node_to_delete);
+		return (1);
+	}
 	while (i < index - 1 && helper != NULL)
 	{
 		i++;
 		helper = helper->next;
 	}
-	if (helper == NULL && i < index -1)
+	if (helper == NULL || helper->next == NULL)
 		return (-1);
-	nodehelper = helper->next;
-	helper->next->next->prev = helper;
-	helper = helper->next->next;
-	free(nodehelper);
+	node_to_delete = helper->next;
+	helper->next = node_to_delete->next;
+	if (node_to_delete->next != NULL)
+	{
+		node_to_delete->next->prev = helper;
+	}
+	free(node_to_delete);
 	return (1);
 }
