e616d1f2275cdd7ebea339b0091e433e8cee3ca6
nepodude <nepo.jean73@gmail.com>
2024-12-10 10:24:54 +0200

    updated the 5-island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,80 +7,99 @@
 
 def island_perimeter(grid):
     """a function to calculate the perimeter of the island."""
-    perimeter = 0
+    horizontal_perimeter = 0
     counter = 0
     semi_counter = 0
     counted = 0
     sum_counted = 0
-    power_of_array = 0
+    vertical_perimeter = 0
     len_grid = len(grid)
 
+    """if the length of grid is 0"""
     if len_grid == 0:
         return 0
+
+    """if the grid has only one row"""
     if len_grid == 1:
         perimter += sum(item for item in grid[0])
         return perimter
 
+    """if the grid has multicple rows"""
     for item in grid:
+        """for each row"""
         semi_counter = 0
         for element in item:
+            """for each element in a row"""
             if counter == 0:
+                """dealing with the first row"""
                 if element == 1 and grid[1][semi_counter] == 1:
-                    perimeter += 1
+                    horizontal_perimeter += 1
                 if element == 1 and grid[1][semi_counter] == 0:
-                    perimeter += 2
+                    horizontal_perimeter += 2
 
             if counter == len_grid - 1:
+                """dealing with the last row"""
                 if element == 1 and grid[len_grid - 2][semi_counter] == 1:
-                    perimeter += 1
+                    horizontal_perimeter += 1
                 if element == 1 and grid[len_grid - 2][semi_counter] == 0:
-                    perimeter += 2
+                    horizontal_perimeter += 2
 
             if 0 < counter < len_grid - 1:
+                """dealing with all rows in between"""
                 if (
                     element == 1
                     and grid[counter - 1][semi_counter] == 1
                     and grid[counter + 1][semi_counter] == 0
                 ):
-                    perimeter += 1
+                    """When a 1 has a 1 above and a 0 below"""
+                    horizontal_perimeter += 1
                 if (
                     element == 1
                     and grid[counter - 1][semi_counter] == 0
                     and grid[counter + 1][semi_counter] == 1
                 ):
-                    perimeter += 1
+                    """When a 1 has a 0 above and a 1 below"""
+                    horizontal_perimeter += 1
                 if (
                     element == 1
                     and grid[counter - 1][semi_counter] == 0
                     and grid[counter + 1][semi_counter] == 0
                 ):
-                    perimeter += 2
+                    """When a 1 has a 0 below and above"""
+                    horizontal_perimeter += 2
 
             semi_counter += 1
         counter += 1
-        power_of_array += shortner(item)
-    perimeter += power_of_array
-    return perimeter
+        vertical_perimeter += vertical_calculator(item)
+    return horizontal_perimeter + vertical_perimeter
 
 
-def shortner(array):
+def vertical_calculator(array):
+    """Calculates the vertical perimeter of any row"""
     power_of_array = 0
     helper = 0
 
+    """When the array is of length 1"""
     if len(array) == 1:
         return array[0]
 
+    """When the array is composed of ones only"""
     if all(element == 1 for element in array):
         return 2
 
+    """When the array is a usual array"""
     for item in array:
         if helper == 0 and item == 1 and array[1] == 0:
+            """When the first 1 is followed by a 1"""
             power_of_array += 2
         if helper == 0 and item == 1 and array[1] == 1:
+            """When the first 1 is followed by a 0"""
             power_of_array += 1
         if helper == len(array) - 1 and item == 1 and array[helper - 1] == 0:
+            """When the last 1 is preceeded by a 0"""
             power_of_array += 2
         if helper == len(array) - 1 and item == 1 and array[helper - 1] == 1:
+            """When the last 1 is preceeded by a 1"""
             power_of_array += 1
         if (
             0 < helper < len(array) - 1
@@ -88,6 +107,7 @@ def shortner(array):
             and array[helper - 1] == 0
             and array[helper + 1] == 0
         ):
+            """when the 1 is surrounded by 0's"""
             power_of_array += 2
         if (
             0 < helper < len(array) - 1
@@ -95,6 +115,7 @@ def shortner(array):
             and array[helper - 1] == 1
             and array[helper + 1] == 0
         ):
+            """When the 1 is preceeded by a 1 and followed by a 0"""
             power_of_array += 1
         if (
             0 < helper < len(array) - 1
@@ -102,6 +123,7 @@ def shortner(array):
             and array[helper - 1] == 0
             and array[helper + 1] == 1
         ):
+            """When the 1 is preceeded by a 0 and followed by a 1"""
             power_of_array += 1
         helper += 1
     return power_of_array
--- 0x1C-makefiles/5-main.py
+++ 0x1C-makefiles/5-main.py
@@ -5,5 +5,9 @@
 island_perimeter = __import__('5-island_perimeter').island_perimeter
 
 if __name__ == "__main__":
-    grid = [[1, 1, 1, 1, 1],[1, 1, 1, 1, 1],[1, 1, 1, 1, 1]]
+    grid = [
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1]
+    ]
     print(island_perimeter(grid))
